33f7b7e44dd6bbfa56860fd74d1c3142e9271cc1
HansDussich <81448184+HansDussich@users.noreply.github.com>
2023-03-22 18:04:58 -0500

    Add files via upload

---
+++ ejercicio1_corte2_algotitmosII/ejercicio1_corte2_algotitmosII/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace ejercicio1_corte2_algotitmosII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.listBox1.Font = new System.Drawing.Font("Lucida Console", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(442, 66);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(287, 260);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Leelawadee UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(205, 248);
+            this.button1.Margin = new System.Windows.Forms.Padding(10);
+            this.button1.MinimumSize = new System.Drawing.Size(100, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(145, 80);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Cambiar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(153, 165);
+            this.textBox1.MinimumSize = new System.Drawing.Size(250, 70);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(250, 70);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "  ";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 66);
+            this.label1.MinimumSize = new System.Drawing.Size(250, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 70);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ingrese el valor que desea cambiar";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Cambiar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ejercicio1_corte2_algotitmosII/ejercicio1_corte2_algotitmosII/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejercicio1_corte2_algotitmosII
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float Cantidad = Convert.ToInt16(textBox1.Text);
+            int C100, C50, C20, C10, C5, C1, D50, D10, D01;
+            C100 = 0;
+            C50 = 0;
+            C20 = 0;
+            C10 = 0;
+            C5 = 0;
+            C1 = 0;
+            D50 = 0;
+            D10 = 0;
+            D01 = 0;
+
+            if ((Cantidad >= 100))
+            {
+                C100 = ((int)Cantidad / 100);
+                Cantidad = Cantidad - (C100 * 100);
+            }
+            if ((Cantidad >= 50))
+            {
+                C50 = ((int)Cantidad / 50);
+                Cantidad = Cantidad - (C50 * 50);
+            }
+            if ((Cantidad >= 20))
+            {
+                C20 = ((int)Cantidad / 20);
+                Cantidad = Cantidad - (C20 * 20);
+            }
+            if ((Cantidad >= 10))
+            {
+                C10 = ((int)Cantidad / 10);
+                Cantidad = Cantidad - (C10 * 10);
+            }
+            if ((Cantidad >= 5))
+            {
+                C5 = ((int)Cantidad / 5);
+                Cantidad = Cantidad - (C5 * 5);
+            }
+            if ((Cantidad >= 1))
+            {
+                C1 = ((int)Cantidad / 1);
+                Cantidad = Cantidad - (C1 * 1);
+            }
+            if ((Cantidad >= 0.5))
+            {
+                D50 = (int)(Cantidad / 0.5);
+                Cantidad = Cantidad - (float)(D50 * 0.5);
+            }
+            if ((Cantidad >= 0.1))
+            {
+                D10 = (int)(Cantidad / 0.1);
+                Cantidad = Cantidad - (float)(D10 * 0.1);
+            }
+            if ((Cantidad >= 0.01))
+            {
+                D01 = (int)Math.Round(Cantidad / 0.01);
+                Cantidad = Cantidad - (float)(D01 * 0.01);
+            }
+
+            listBox1.Items.Add("Billetes de 100: " + C100);
+            listBox1.Items.Add("Billetes de 50: " + C50);
+            listBox1.Items.Add("Billetes de 20: " + C20);
+            listBox1.Items.Add("Billetes de 10: " + C10);
+            listBox1.Items.Add("Billetes de 5: " + C5);
+            listBox1.Items.Add("Monedas de 1: " + C1);
+            listBox1.Items.Add("Monedas de 0.50: " + D50);
+            listBox1.Items.Add("Monedas de 0.10: " + D10);
+            listBox1.Items.Add("Monedas de 0.01: " + D01);
+        }          
+    }
+}
